a11e31a7c2de0e18bdc53e3c087cf53a8f402546
dani <martinelli.daniele7@gmail.com>
2025-01-05 15:51:17 +0100

    run http server for heroku

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import WebSocket from 'ws';
 import { connectNDK, subscribeToEvent } from './utils/nostr';
 import { COINFLIP_NDK_FILTER, createCoinflip } from './modules/coinflip';
+import http from 'http';
 
 // @ts-ignore
 global.WebSocket = WebSocket;
 
 const main = async () => {
   const ndk = await connectNDK();
-  console.log('Connected to NDK');
+  console.log('🔌 Connected to NDK');
 
   subscribeToEvent(ndk, {
     filters: COINFLIP_NDK_FILTER,
@@ -16,4 +17,14 @@ const main = async () => {
   });
 };
 
-main();
+// Add an HTTP server to bind to $PORT (required by Heroku)
+const PORT = process.env.PORT || 3000; // Fallback to 3000 for local testing
+
+http
+  .createServer((_, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Nostr Game backend is running!\n');
+  })
+  .listen(PORT, () => {
+    main();
+  });
